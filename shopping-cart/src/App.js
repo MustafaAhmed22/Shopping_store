0f5a623b015016d5e50ecc9eb4f9870f0ff630e2
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2020-12-22 02:32:39 +0200

    a

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,7 +1,13 @@
 
 function App() {
   return (
-   <div className='grid-container'></div>
+   <div className='grid-container'>
+   <header>
+   <a href='/'>React shopping cart</a>
+   </header>
+   <main>product list</main>
+   <footer>all right is reserved .</footer>
+   </div>
   );
 }
 
